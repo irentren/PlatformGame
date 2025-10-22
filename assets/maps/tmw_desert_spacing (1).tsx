791035786a8d30522a24084be52f4dd2c263bb2b
irentren <icalatravasierra@gmail.com>
2025-10-22 16:27:14 +0200

    sss

---
+++ assets/maps/tmw_desert_spacing (1).tsx
@@ -0,0 +1,87 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tmw_desert_spacing" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="48" columns="8">
+ <image source="tmw_desert_spacing (1).png" width="265" height="199"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.875" y="16" width="16.125" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="16" width="32" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="16" width="16.125" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.625" y="0" width="16.375" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="18" height="32"/>
+   <object id="2" x="0" y="0" width="16.375" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.75" y="0" width="16.25" height="16.625"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="17.125"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16.125" height="18.125"/>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.125" y="0.125" width="32" height="15.875"/>
+   <object id="2" x="6" y="22.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="9.875" y="6.125"/>
+   <object id="2" x="16.375" y="0" width="16" height="33.625"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0.125" width="16.125" height="31.875"/>
+   <object id="2" x="22.25" y="25.625"/>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.75" y="0" width="16.25" height="32"/>
+   <object id="2" x="9" y="26"/>
+  </objectgroup>
+ </tile>
+ <wangsets>
+  <wangset name="Unnamed Set" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="8" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="9" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="10" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="16" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="17" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="18" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="19" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="20" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="27" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="28" wangid="0,1,0,1,0,1,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
